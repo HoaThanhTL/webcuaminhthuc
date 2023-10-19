7dd95ad6fbbf6a85cde250792e8d7c5920da0aea
Admin <Admin@DESKTOP-6FBTGHT>
2023-10-19 23:38:35 +0700

    VOTHIMINHTHUC

---
+++ web/script.js
@@ -0,0 +1,29 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+const menuBar = document.querySelector(".menu-bar");
+menuBar.addEventListener("click", function(){
+    menuBar.classList.toggle("active")
+    document.querySelector(".menu-items").classList.toggle("active")
+});
+const toP = document.querySelector(".top");
+window.addEventListener("scroll", function(){
+    const x = this.pageYOffset;
+    if(x>80){toP.classList.add("active")}
+    else{toP.classList.remove("active")}
+})     
+const menuTitle = document.querySelector(".menu-title");
+menuTitle.addEventListener("click",function(x){
+    if(x.target.classList.contains("menu-button")){
+        const Target = x.target.getAttribute("data-title");
+        //console.log(Target)
+        menuTitle.querySelector(".acvite").classList.remove("active");
+        x.target.classList.add("active");
+        
+        const menuItem = document.querySelector(".menu");
+        menuItem.querySelector(".menu-item-content active").classList.remove("active");
+        menuItem.querySelector(Target).classList.add("active");
+    }
+})
+
